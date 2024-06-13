149f440369970ee726db3508a7faffbda47a4cd6
ostossticker <info755755@gmail.com>
2024-06-13 11:13:57 +0700

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:["openweathermap.org","lh3.googleusercontent.com","avatars.githubusercontent.com","bankerjobs.asia","ostospos.s3.ap-southeast-1.amazonaws.com","ostoss3.s3.ap-southeast-2.amazonaws.com"]
+    },
+    experimental:{
+        serverActions:true
+    }
+};
 
 export default nextConfig;
