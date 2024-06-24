6e7e36a8aa7659a9c4a25bf7ea62304f9c707dd3
ostossticker <info755755@gmail.com>
2024-06-24 10:56:14 +0700

    changes

--- lib/mail.ts
+++ lib/mail.ts
@@ -15,9 +15,9 @@ export const sendVerificationEmail = async (
     email:string,
     token:string
 ) =>{
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+    const confirmLink = `/auth/new-verification?token=${token}`
     await resend.emails.send({
-        from:"onboarding@resend.dev",
+        from:"info755755@gmail.com",
         to:email,
         subject:"Confirm you email",
         html:`<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
@@ -28,9 +28,9 @@ export const sendPasswordResetEmail = async (
     email:string,
     token:string
 ) =>{
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+    const resetLink = `/auth/new-password?token=${token}`
     await resend.emails.send({
-        from:"onboarding@resend.dev",
+        from:"info755755@gmail.com",
         to:email,
         subject:"Reset your password",
         html:`<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
